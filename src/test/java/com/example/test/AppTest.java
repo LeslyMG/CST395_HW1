a33fc34603287665645a32c16bd2e77a8615343c
LeslyMG <lesly1995ga@gmail.com>
2015-02-25 21:04:03 +0000

    Add modified files

--- src/test/java/com/example/test/AppTest.java
+++ src/test/java/com/example/test/AppTest.java
@@ -4,11 +4,14 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
+        extends TestCase
 {
     /**
      * Create the test case
@@ -31,8 +34,43 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testAllMembersInSameGroup() throws Exception
     {
-        assertTrue( true );
+        List<String> names = new ArrayList<String>();
+        names.add("a");
+        names.add("b");
+        names.add("c");
+        names.add("d");
+        names.add("e");
+        names.add("f");
+        App app = new App(names);
+        List<String> actualA = app.createGroup("a");
+        List<String> actualB = app.createGroup("b");
+        List<String> actualC = app.createGroup("c");
+        List<String> expected = new ArrayList<String>();
+        expected.add("a");
+        expected.add("b");
+        expected.add("c");
+        assertEquals(actualA, expected);
+        assertEquals(actualB, expected);
+        assertEquals(actualC, expected);
+    }
+
+    public void testSecondGroup() throws Exception
+    {
+        List<String> names = new ArrayList<String>();
+        names.add("a");
+        names.add("b");
+        names.add("c");
+        names.add("d");
+        names.add("e");
+        names.add("f");
+        App app = new App(names);
+        List<String> actual = app.createGroup("e");
+        List<String> expected = new ArrayList<String>();
+        expected.add("d");
+        expected.add("e");
+        expected.add("f");
+        assertEquals(actual, expected);
     }
 }
